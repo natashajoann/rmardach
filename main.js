7b3f56f3cdd4baddba82a5198cd18d6cd207686c
Natasha Verdon <natashamverdon@gmail.com>
2020-08-18 12:20:18 +0200

    burger attempt

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
   $(".burger-button").click(function(){
-    $(".burger-button").toggleClass("active");
+    $(".burger-button").toggleClass("activate");
     $(".burger-menu").slideToggle();
   });
 
 
   $(".burger-menu").click(function(){
-    $(".burger-button").toggleClass("active");
+    $(".burger-button").toggleClass("activate");
     $(".burger-menu").slideToggle();
   });
 
