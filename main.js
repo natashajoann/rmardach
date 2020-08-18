f182c8c367b29cfe4460289022d1932f6ff96965
Natasha Verdon <natashamverdon@gmail.com>
2020-08-18 11:19:38 +0200

    accordion and menu

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
+$(document).ready(function() {
+
+  $(".burger-button").click(function(){
+    $(".burger-button").toggleClass("active");
+    $(".burger-menu").slideToggle();
+  });
+
+
+  $(".burger-menu").click(function(){
+    $(".burger-button").toggleClass("active");
+    $(".burger-menu").slideToggle();
+  });
+
+  $(".burger-button").click(function(){
+    $(".burger-menu").toggleClass("active");
+  });
+
+//
+
+
+  $( document ).ready(function() {
+  $(".dropdown-nav").click(function(){
+      $(this).children(".dropdown-content").slideToggle("200");
+    });
+  });
+
 $( document ).ready(function() {
-  // BURGER menu
+$(".dropdown-nav").click(function(){
+    $(this).children(".dropdown-content").slideToggle("200");
+  });
+});
 
-     $(".burger-button").click(function(){
-       $(".burger-button").toggleClass("active");
-       $(".burger-menu").slideToggle();
-     });
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
 
-     $(".burger-menu").click(function(){
-       $(".burger-button").toggleClass("active");
-       $(".burger-menu").slideToggle();
-     });
-
-     $(".burger-menu").click(function(){
-       $(".burger-menu").toggleClass("active");
-     });
-     
-   var acc = document.getElementsByClassName("accordion");
-     var i;
-
-     for (i = 0; i < acc.length; i++) {
-       acc[i].addEventListener("click", function() {
-         this.classList.toggle("active");
-         var panel = this.nextElementSibling;
-         if (panel.style.maxHeight) {
-           panel.style.maxHeight = null;
-         } else {
-           panel.style.maxHeight = panel.scrollHeight + "px";
-         }
-       });
-     }
+});
