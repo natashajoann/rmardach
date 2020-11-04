cfd38553ed7f24a3a9d9bcaa004c4ccb161540f4
Natasha Verdon <natashamverdon@gmail.com>
2020-11-04 08:11:41 +0100

    mom-menu

--- main.js
+++ main.js
@@ -1,36 +1,6 @@
 $(document).ready(function() {
 
-  $(".burger-button").click(function(){
-    $(".burger-button").toggleClass("activate");
-    $(".burger-menu").slideToggle();
-  });
-
-
-  $(".burger-menu").click(function(){
-    $(".burger-button").toggleClass("activate");
-    $(".burger-menu").slideToggle();
-  });
-
-  $(".burger-button").click(function(){
-    $(".burger-menu").toggleClass("activate");
-  });
-
-//
-
-
-  $( document ).ready(function() {
-  $(".dropdown-nav").click(function(){
-      $(this).children(".dropdown-content").slideToggle("200");
-    });
-  });
-
-$( document ).ready(function() {
-$(".dropdown-nav").click(function(){
-    $(this).children(".dropdown-content").slideToggle("200");
-  });
-});
-
-    var acc = document.getElementsByClassName("accordion");
+  var acc = document.getElementsByClassName("accordion");
     var i;
 
     for (i = 0; i < acc.length; i++) {
@@ -45,4 +15,18 @@ $(".dropdown-nav").click(function(){
       });
     }
 
-});
+  $('.hamburger__container').click(function() {
+              $('.nav').toggleClass('open');
+              $(this).children().first().toggleClass('open');
+              $('html, body').toggleClass('noscroll');
+          });
+
+          // Bacon Ipsum
+          $.ajax({
+              url: 'https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1&format=html',
+              success: function(data) {
+                  $('#content').html(data);
+              }
+          });
+
+ });
